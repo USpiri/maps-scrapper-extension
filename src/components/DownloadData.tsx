7f4d4d90d8c945d29b7954f113fdd13bb933c6e2
USpiri <urielspiridione135@gmail.com>
2024-10-01 18:16:27 -0300

    ✨ Feat: Download json

--- src/components/DownloadData.tsx
+++ src/components/DownloadData.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { MapItem } from "../models/item.interface";
 import { Button } from "./ui/Button";
-import { downloadMapItemsCSV } from "../utils/download-csv";
+import {
+  downloadMapItemsCSV,
+  downloadMapItemsJSON,
+} from "../utils/download-csv";
 
 interface DownloadDataProps {
   data: MapItem[];
@@ -9,7 +12,8 @@ interface DownloadDataProps {
 
 export const DownloadData = ({ data }: DownloadDataProps) => {
   const [fileName, setFileName] = useState("maps-data");
-  const onDownload = downloadMapItemsCSV(data, fileName);
+  const onDownloadCSV = downloadMapItemsCSV(data, fileName);
+  const onDownloadJSON = downloadMapItemsJSON(data, fileName);
 
   const onInputChange = (value: string) => {
     setFileName(value);
@@ -20,13 +24,18 @@ export const DownloadData = ({ data }: DownloadDataProps) => {
       className="flex gap-3"
       onSubmit={(e) => {
         e.preventDefault();
-        onDownload();
+        onDownloadCSV();
       }}
     >
-      <Button type="submit" variant="primary">
-        Download CSV
-      </Button>
-      <label>
+      <div className="flex flex-col">
+        <Button type="submit" variant="primary">
+          Download CSV
+        </Button>
+        <Button type="button" variant="primary" onClick={onDownloadJSON}>
+          Download JSON
+        </Button>
+      </div>
+      <label className="flex-1">
         <span>File Name:</span>
         <input
           placeholder="file-name.csv"
--- src/utils/download-csv.ts
+++ src/utils/download-csv.ts
@@ -34,8 +34,26 @@ export const downloadMapItemsCSV =
     ]
       .map((row) => row.join(","))
       .join("\n");
+    data.forEach((i) => {
+      console.log(i);
+    });
 
     const blob = new Blob([csvValues], { type: "text/csv" });
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
+
+export const downloadMapItemsJSON =
+  (data: MapItem[], fileName: string) => () => {
+    const json = JSON.stringify(data, null, 2);
+    const blob = new Blob([json], { type: "application/json" });
 
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
